5a97909b615d0b260a7b7eb7cf40e5788ee9fac5
James Perkins <jamesperkins@hey.com>
2022-06-28 09:56:30 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Tina from '../.tina/components/TinaDynamicProvider.js';
-import Script from 'next/script.js';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Navbar } from '../components/Layout/NavBar/Navbar';
 import '../utils/video-player.css';
@@ -8,11 +7,6 @@ import Footer from '../components/Layout/Footer/footer.jsx';
 const App = ({ Component, pageProps }) => {
     return (
         <ChakraProvider theme={theme}>
-            <Script
-                src="https://plausible.io/js/plausible.js"
-                strategy="worker"
-                data-domain="jamesperkins.dev"
-            />
             <Tina>
                 <Navbar />
                 <Component {...pageProps} />
